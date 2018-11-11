daa9ab8802ebe791f6c765abd077496b455252fe
Bc. Michal Fecenko <michal.fecenko@student.tuke.sk>
2018-11-11 20:58:27 +0100

    updated script to use original yajco-examples project

--- test_examples.sh
+++ test_examples.sh
@@ -4,6 +4,6 @@ mvn install package
 #find current yajco version
 yajco_version=`mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version | grep -v '\['`
 #clone yajco-examples and execute tests
-git clone --depth=1 https://github.com/mfecenko/yajco-examples
+git clone --depth=1 https://github.com/kpi-tuke/yajco-examples
 cd yajco-examples
 mvn verify -Dyajco.version=$yajco_version
