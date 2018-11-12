43bed852a3885d88ecc55afe35b874a1855146e6
Sergej Chodarev <sergejx@sergejx.net>
2018-11-12 10:54:45 +0100

    Fix command for getting project version
    The problem was with the grep command trying to extract version from
    the output of Maven. When Maven downloaded dependencies, it included
    them in the result. Corrected script suppresses all output except of
    the version number, so grep is not needed. Solution is based on
    the blog post: https://blog.soebes.de/blog/2018/06/09/help-plugin/

--- test_examples.sh
+++ test_examples.sh
@@ -2,7 +2,8 @@
 
 mvn install package
 #find current yajco version
-yajco_version=`mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version | grep -v '\['`
+yajco_version=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate \
+                -Dexpression=project.version -q -DforceStdout)
 #clone yajco-examples and execute tests
 git clone --depth=1 https://github.com/kpi-tuke/yajco-examples
 cd yajco-examples
